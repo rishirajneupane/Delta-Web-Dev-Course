18b7a683849625dbbcceb580fff5cb19cc25bd5d
rishi <rishirajneupane0125@gmail.com>
2024-04-04 11:08:21 +0545

    code refactoring

--- javascript/basics/favMovie/app.js
+++ javascript/basics/favMovie/app.js
@@ -1,11 +1,15 @@
 let favMovie = "avatar";
 let guess = prompt("Guess my favourite movie");
 
-while (guess != favMovie && guess != "quit") {
+while (guess != favMovie) {
+  if (guess == "quit") {
+    console.log("Bye!");
+    break;
+  }
+
   guess = prompt("Wrong! try again.");
 }
+
 if (guess == favMovie) {
   console.log("You win ");
-} else {
-  console.log("Bye!");
 }
