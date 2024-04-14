14789134d90857c018bbac2ecc473dc986c8a726
rishi <rishirajneupane0125@gmail.com>
2024-04-14 21:44:19 +0545

    modifying custom text editor

--- javascript/customtextetEditor/script.js
+++ javascript/customtextetEditor/script.js
@@ -1,7 +1,15 @@
-let input = document.querySelector("input");
-let h1 = document.querySelector("h1");
-input.addEventListener("input", function (evt) {
-//   evt.preventDefault();
-  console.log(input.value);
-  h1.innerText = input.value;
+document.addEventListener("DOMContentLoaded", () => {
+  const editor = document.getElementById("editor");
+  const h1 = document.querySelector("h1");
+
+  editor.addEventListener("input", () => {
+    h1.innerText = editor.value;
+    localStorage.setItem("editorText", editor.value);
+  });
+
+  const savedText = localStorage.getItem("editorText");
+  if (savedText) {
+    editor.value = savedText;
+    h1.innerText = savedText;
+  }
 });
